e3ecbe5a9a7e646d068d0eeb2b63dead1b1e5d6a
Rafael Rier <rafael.riber@epfl.ch>
2024-10-24 17:21:51 +0200

    Initial commit

---
+++ src/uv_amaranth/__init__.py
@@ -0,0 +1,38 @@
+from amaranth import *
+
+from amaranth_boards.icebreaker import ICEBreakerPlatform
+from amaranth_boards.versa_ecp5 import VersaECP5Platform
+from amaranth_boards.tang_nano import TangNanoPlatform
+from amaranth_boards.arty_a7 import ArtyA7_100Platform
+
+from .blinky import Blinky
+
+from dotenv import load_dotenv
+
+class Toplevel(Elaboratable):
+    def elaborate(self, platform):
+        m = Module()
+
+        m.submodules.blinky = blinky = Blinky(frequency=platform.default_clk_frequency)
+        m.d.comb += platform.request("led", 0).o.eq(blinky.led)
+
+        return m
+
+
+def build_ice40():
+    load_dotenv()
+    ICEBreakerPlatform().build(Toplevel())
+
+
+def build_ecp5():
+    load_dotenv()
+    VersaECP5Platform().build(Toplevel())
+
+
+def build_gowin():
+    load_dotenv()
+    TangNanoPlatform().build(Toplevel())
+
+def build_arty100():
+    load_dotenv()
+    ArtyA7_100Platform().build(Toplevel())
